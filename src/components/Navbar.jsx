c9fbc7aca43faee2cf9ee51ed5a979de09047a01
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-11-18 23:19:03 +0530

    Modified Cart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect, useRef } from 'react';
 import { CartContext } from './CartContext';
 import Cart from './Cart';
 import './Navbar.css';
@@ -6,12 +6,28 @@ import './Navbar.css';
 function Navbar() {
   const { cart } = useContext(CartContext);
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const cartRef = useRef(null);
 
   const toggleCart = () => {
     setIsCartOpen(!isCartOpen);
   };
 
   const cartItemCount = cart.reduce((total, item) => total + item.quantity, 0);
+
+  useEffect(() => {
+ 
+    const handleClickOutside = (event) => {
+      if (cartRef.current && !cartRef.current.contains(event.target)) {
+        setIsCartOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className="navContainer">
@@ -24,8 +40,8 @@ function Navbar() {
         </li>
       </ul>
       {isCartOpen && (
-        <div className="cart-dropdown">
-          <Cart /> 
+        <div className="cart-dropdown" ref={cartRef}>
+          <Cart />
         </div>
       )}
     </div>
